caddf36458e34bfa9e2d6472c8891ff4b509dbd3
Tanmoy Sarker <tanmoysarker749@outlook.com>
2019-05-14 11:41:51 +0600

    first prizes working

--- api/routes/magnum.js
+++ api/routes/magnum.js
@@ -4,21 +4,31 @@ const cheerio = require('cheerio');
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
-  const resultTable = []
+  let resultTable = []
   request('https://www.check4d.com/', (error, response, html) => {
     if (!error && response.statusCode == 200) {
       const $ = cheerio.load(html);
-      $('.resultTable').each((i, el) => {
+      $('#magnum4d').each((i, el) => {
         let resultTop = $(el).find('.resulttop').text()
         resultTop = resultTop.match(/.{1,4}/g)
-        resultTable.push(resultTop)
+        resultTop = resultTop.map((r, index) => {
+          if (index === 0) {
+            return ['1ST Prize', r]
+          } else if (index === 1) {
+            return ['2ND Prize', r]
+          } else if (index === 2) {
+            return ['3RD Prize', r]
+          }
+          console.log(index+':', r)
+        })
+        resultTable = resultTop
         // let resultBottom = $(el).find('.resultbottom').text()
         // resultTable.push(resultBottom.match(/.{1,4}/g))
         // resultTable.push($(el).find('.resultbottom') .text())
       })
     }
     res.status(200).json({
-      message: resultTable
+      magnum: resultTable
     });
   });
 
