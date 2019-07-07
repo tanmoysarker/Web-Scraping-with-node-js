c2ec9ff879c0e1ad03c3dac89871bce6acb7f1aa
Tanmoy Sarker <tanmoysarker749@outlook.com>
2019-07-07 15:32:01 +0600

    minor change

--- api/newRoutes/nine932.js
+++ api/newRoutes/nine932.js
@@ -14,19 +14,28 @@ router.get('/:id?', (req, res, next) => {
   let special = []
   let consolidation = []
   let second = []
+  let result6 = []
   
   request(url, (error, response, html) => {
     if (!error && response.statusCode == 200) {
       const $ = cheerio.load(html);
         let list = $("body");
         let strarray = list.html()
+        let pos = strarray.search("#1#")
+        let f = strarray.slice(pos+3,pos+7)
+        let pos1 = strarray.search("#2#")
+        let s = strarray.slice(pos1+3,pos1+7)
+        let pos2 = strarray.search("#3#")
+        let t = strarray.slice(pos2+3,pos2+7)
+        result3.push(f,s,t)
+        firstPhase.push(result3)
         let date = strarray.slice(64,72)
          let year = date.slice(0,4)
          let d = date.slice(4,6)
          let month = date.slice(6,8) 
          result.push(d,month,year)
          strarray = strarray.split("#"); 
-         
+        
           strarray.forEach(function (value, i) {
           if((i+1) % 3 === 0){
             let resultBottom = value.split(',')
@@ -36,28 +45,29 @@ router.get('/:id?', (req, res, next) => {
       });
       
     }
-  let firstName = result1[0][0]
+  let first = result3[0]
+  let a = result1.indexOf(first)
+  console.log(a)
+  let firstName = result3[0]
   let firstP = ['1st Prize',firstName]
-  let secondName = result1[1][0]
+  let secondName = result3[1]
   let secondP = ['2nd Prize',secondName]
-  let thirdName = result1[2][0]
+  let thirdName = result3[2]
   let thirdP = ['3rd Prize',thirdName]
-  firstPhase.push(firstName,secondName,thirdName)
-  result3.push(firstPhase)
   second.push(firstP,secondP,thirdP)
   
-  let secondPhase1 = [].concat.apply([], result1).slice(3,8)
-  let secondPhase2 = [].concat.apply([], result1).slice(8,13)
+  let secondPhase1 = [].concat.apply([], result1).slice(0,5)
+  let secondPhase2 = [].concat.apply([], result1).slice(5,10)
   special.push(secondPhase1,secondPhase2)
   
-  let thirdPhase1 = [].concat.apply([], result1).slice(13,18)
-  let thirdPhase2 = [].concat.apply([], result1).slice(18,23)
+  let thirdPhase1 = [].concat.apply([], result1).slice(10,15)
+  let thirdPhase2 = [].concat.apply([], result1).slice(15,20)
   consolidation.push(thirdPhase1,thirdPhase2)
 
     res.status(200).json({
       All: result1,
       Date: result,
-      First: result3,
+      First: firstPhase,
       First2: second,
       Special: special,
       Consolidation: consolidation
