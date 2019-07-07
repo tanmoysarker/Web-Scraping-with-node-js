c130897caaeacf17518a0f3367067dec6f9c6c79
Tanmoy Sarker <tanmoysarker749@outlook.com>
2019-07-07 21:11:34 +0600

    updated2

--- api/newRoutes/magnum2.js
+++ api/newRoutes/magnum2.js
@@ -35,58 +35,61 @@ router.get('/:id?', (req, res, next) => {
         let resultBottomSecond = $(el).find('.resultbottom').text()
         resultTable7.push(resultBottomSecond)
       })
-      let first = resultTable2[0].toString()
-      let date = first.slice(18, 28)
-      let draw = first.slice(34, 49)
-      let firstPrize = first.slice(61, 65)
-      let firstP = ['1st Prize',firstPrize ]
-      let secondPrize = first.slice(77, 81)
-      let secondP = ['2nd Prize',secondPrize ]
-      let thirdPrize = first.slice(93, 97)
-      let thirdP = ['3rd Prize',thirdPrize ]
-      resultTable = resultTable2[0]
-      resultTable3.push(firstPrize, secondPrize, thirdPrize)
-      fr.push(resultTable3)
-      sc.push(firstP,secondP,thirdP)
+      console.log(resultBottomFirst)
+      if (resultTable2[0] === undefined) {
+        let first = resultTable2[0].toString()
+        let date = first.slice(18, 28)
+        let draw = first.slice(34, 49)
+        let firstPrize = first.slice(61, 65)
+        let firstP = ['1st Prize',firstPrize ]
+        let secondPrize = first.slice(77, 81)
+        let secondP = ['2nd Prize',secondPrize ]
+        let thirdPrize = first.slice(93, 97)
+        let thirdP = ['3rd Prize',thirdPrize ]
+        resultTable = resultTable2[0]
+        resultTable3.push(firstPrize, secondPrize, thirdPrize)
+        fr.push(resultTable3)
+        sc.push(firstP,secondP,thirdP)
 
-      mdate.push(date) 
-      mdraw.push(draw)
+        mdate.push(date) 
+        mdraw.push(draw)
 
-      resultTable5 = resultTable4[0]
-      let middle = resultTable4[0].toString()
-      let resultBottom1 = middle.match(/.{1,4}/g).slice(0,5)
-      let resultBottom2 = middle.match(/.{1,4}/g).slice(5,10)
-      let resultBottom3 = middle.match(/.{1,4}/g).slice(10,13)
-      resultTable6.push(resultBottom1,resultBottom2,resultBottom3) 
+        resultTable5 = resultTable4[0]
+        let middle = resultTable4[0].toString()
+        let resultBottom1 = middle.match(/.{1,4}/g).slice(0,5)
+        let resultBottom2 = middle.match(/.{1,4}/g).slice(5,10)
+        let resultBottom3 = middle.match(/.{1,4}/g).slice(10,13)
+        resultTable6.push(resultBottom1,resultBottom2,resultBottom3) 
 
-      resultTable8 = resultTable7[0]
-      let last = resultTable7[0].toString()
-      let resultBottom4 = last.match(/.{1,4}/g).slice(13,18)
-      let resultBottom5 = last.match(/.{1,4}/g).slice(18,23)
-      resultTable9.push(resultBottom4, resultBottom5)
+        resultTable8 = resultTable7[0]
+        let last = resultTable7[0].toString()
+        let resultBottom4 = last.match(/.{1,4}/g).slice(13,18)
+        let resultBottom5 = last.match(/.{1,4}/g).slice(18,23)
+        resultTable9.push(resultBottom4, resultBottom5)
+      }
     }
     if (mdate === undefined || mdate.length == 0) {
-      res.status(200).json({
+      res.status(404).json({
         error: "data not found",
       });
     } else if (mdraw === undefined || mdraw.length == 0) {
-      res.status(200).json({
+      res.status(404).json({
         error: "data not found",
       });
     } else if (fr === undefined || fr.length == 0) {
-      res.status(200).json({
+      res.status(404).json({
         error: "data not found",
       });
     } else if (sc === undefined || sc.length == 0) {
-      res.status(200).json({
+      res.status(404).json({
         error: "data not found",
       });
     } else if (resultTable6 === undefined || resultTable6.length == 0) {
-      res.status(200).json({
+      res.status(404).json({
         error: "data not found",
       });
     } else if (resultTable9 === undefined || resultTable9.length == 0) {
-      res.status(200).json({
+      res.status(404).json({
         error: "data not found",
       });
     } else {
